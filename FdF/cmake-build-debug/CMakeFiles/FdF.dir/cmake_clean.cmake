812936a5998107837a9d3d88b765aee519953525
Илья <bold-hen@MacBook-Pro-Ila.local>
2019-02-06 11:24:31 +0300

    новые проекты, еще не начатые

---
+++ FdF/cmake-build-debug/CMakeFiles/FdF.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/FdF.dir/main.c.o"
+  "FdF.pdb"
+  "FdF"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/FdF.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ FdF/main.c
@@ -0,0 +1,7 @@
+#include "FdF.h"
+
+int main(int argc, char **argv)
+{
+
+    return 0;
+}
