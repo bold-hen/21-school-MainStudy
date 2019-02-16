ea2b888909ffc4888ff9420d7afa4e793597c83e
bold-hen <bold-hen@mail.ru>
2019-02-16 19:39:31 +0300

    Добавил список клавиш по номерам

--- FdF/main.c
+++ FdF/main.c
@@ -27,32 +27,10 @@ typedef struct  s_line
 
 int deal_key(int key, void *draw)
 {
-    if (key == 124)
-    {
-        g_x += 4;
-        //right
-    }
-    if (key == 125)
-    {
-        g_y += 4;
-        //bottom
-    }
-    if (key == 126)
-    {
-        g_y -= 4;
-        //top
-    }
-    if (key == 123)
-    {
-        g_x -= 4;
-        //left
-    }
     if (key == 53)
     {
         exit(0);
     }
-    mlx_clear_window(((t_draw *)draw)->mlx_ptr, ((t_draw *)draw)->win_ptr);
-    mlx_pixel_put(((t_draw *)draw)->mlx_ptr, ((t_draw *)draw)->win_ptr, g_x, g_y, 0xFFFFFF);
     return (0);
 }
 
@@ -78,7 +56,6 @@ int main(int argc, char **argv)
     draw = (t_draw *)malloc(sizeof(t_draw));
     mlx_ptr = mlx_init();
     win_ptr = mlx_new_window(mlx_ptr, 300, 300, "test");
-    mlx_pixel_put(mlx_ptr, win_ptr, g_x, g_y, 0xFFFFFF);
     draw->mlx_ptr = mlx_ptr;
     draw->win_ptr = win_ptr;
     mlx_key_hook(win_ptr, deal_key, draw);
